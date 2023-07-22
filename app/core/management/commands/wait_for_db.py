61bec296b16fc77f88e8b40acdd95069e7df0c52
jenkins <jenkins@prft.com>
2023-07-22 14:33:12 +0530

    updated and fixed wait for db command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,26 +1,48 @@
-"""
-Django command to wait for the database to be available
-"""
 import time
 
 from django.core.management.base import BaseCommand
+from django.db import connections
 from django.db.utils import OperationalError
 from psycopg2 import OperationalError as Psycopg2Error
 
 
 class Command(BaseCommand):
-    """Django command to wait for database."""
-    help = "Django command to wait for database"
+    """Django command to wait for the database."""
+    help = "Django command to wait for the database"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--retries', type=int, default=60,
+                            help='Maximum number of retries \
+                                to wait for the database.')
+        parser.add_argument('--retry-delay', type=int, default=5,
+                            help='Delay (in seconds) between retries.')
 
     def handle(self, *args, **options):
-        """Entrypoint for command"""
-        self.stdout.write("Waiting for database...")
-        db_up = False
-        while db_up is False:
+        """Entrypoint for the command."""
+        self.stdout.write("Waiting for the database...")
+
+        max_retries = options['retries']
+        retry_delay = options['retry_delay']
+
+        for retry in range(1, max_retries + 1):
             try:
-                self.check(databases=['default'])
-                db_up = True
-            except (Psycopg2Error, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second...')
-                time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+                for db_alias in connections:
+                    self.check_database_connection(db_alias)
+                self.stdout.write(self.style.SUCCESS('Database available!'))
+                return
+
+            except (OperationalError, Psycopg2Error) as ex:
+                self.stdout.write(f"Attempt {retry}/{max_retries}: {ex}")
+                time.sleep(retry_delay)
+
+        self.stdout.write(self.style.ERROR(
+            f"Database not available after {max_retries} retries."))
+        raise SystemExit(1)
+
+    def check_database_connection(self, db_alias):
+        """Check the database connection."""
+        conn = connections[db_alias]
+        c = conn.cursor()
+        c.execute("SELECT 1")
+        c.fetchone()
+        c.close()
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,21 +9,23 @@ from django.test import SimpleTestCase
 from psycopg2 import OperationalError as Psycopg2Error
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch(
+    'core.management.commands.wait_for_db.Command.check_database_connection')
 class CommandTests(SimpleTestCase):
     """Test Commands"""
 
-    def test_wait_for_db_ready(self, patched_check):
+    def test_wait_for_db_ready(self, patched_check_database_connection):
         """Test waiting for database if database is ready"""
-        patched_check.return_value = True
+        patched_check_database_connection.return_value = True
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(databases=['default'])
+        patched_check_database_connection.assert_called_once_with('default')
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+    def test_wait_for_db_delay(self, patched_sleep,
+                               patched_check_database_connection):
         """Test waiting for database delay"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        patched_check_database_connection.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
         call_command('wait_for_db')
-        self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        self.assertEqual(patched_check_database_connection.call_count, 6)
+        patched_check_database_connection.assert_called_with('default')
