c7fd372fdb7911e09298647a0f682a94349e805c
jenkins <jenkins@prft.com>
2023-07-22 12:59:39 +0530

    added custom user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,52 @@
+"""
+Test for Core models.
+"""
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful"""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@Example.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            password = 'testpass123'
+            user = get_user_model().objects.create_user(
+                email=email,
+                password=password,
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_super_user_with_email_successful(self):
+        """Test creating a super user with an email is successful"""
+        email = 'superuser@example.com'
+        password = 'test123'
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
